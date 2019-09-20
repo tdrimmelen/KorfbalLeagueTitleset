7f9c2341b0ca73644e955ed50331dc9877ff6080
Theo van Drimmelen <theo.vandrimmelen@solvinity.com>
2019-09-20 22:25:20 +0200

    Add project files.

---
+++ EventBottomHome/UserControl1.xaml.cs
@@ -0,0 +1,78 @@
+﻿using DigitsViewLib;
+using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using vMixInterop;
+
+namespace vMixControlLibrary
+{
+    public partial class ScoreboardControl : UserControl, vMixWPFUserControl, IComponentConnector
+    {
+        private string homePrimaryColor;
+        private string homeSecundaryColor;
+     
+
+        public ScoreboardControl()
+        {
+            this.InitializeComponent();
+            Logger.Source = "Scoreboard";
+            string configFilename = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Scoreboard.config";
+            try
+            {
+                Configuration configuration = new Configuration(configFilename);
+                this.homePrimaryColor = configuration[nameof(homePrimaryColor)];
+                this.homeSecundaryColor = configuration[nameof(homeSecundaryColor)];
+            }
+            catch (Exception ex)
+            {
+                Logger.Log("Could not read config file or property not present'" + configFilename + "': " + ex.Message, EventLogEntryType.Error, 1);
+            }
+            if (this.homePrimaryColor != "")
+                this.TEAMCOLOR.Fill = (Brush)new BrushConverter().ConvertFrom((object)this.homePrimaryColor);
+            if (!(this.homeSecundaryColor != ""))
+                return;
+            this.TEAM2NDCOLOR.Background = (Brush)new BrushConverter().ConvertFrom((object)this.homeSecundaryColor);
+        }
+
+        public void Close()
+        {
+        }
+
+        public TimeSpan GetPosition()
+        {
+            return new TimeSpan(0, 0, 0);
+        }
+
+        public TimeSpan GetDuration()
+        {
+            return new TimeSpan(0, 0, 0);
+        }
+
+        public void Load(int width, int height)
+        {
+        }
+
+        public void Pause()
+        {
+        }
+
+        public void Play()
+        {
+        }
+
+        public void SetPosition(TimeSpan position)
+        {
+        }
+
+        public void ShowProperties()
+        {
+        }
+    }
+}
